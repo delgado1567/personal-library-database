33c980e348731af51758e3e602686c71fb444c1a
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-26 23:35:53 -0400

    Update mysql_generation.sql Correct capitalization on NOT NULL constraints.

--- MySQL/mysql_generation.sql
+++ MySQL/mysql_generation.sql
@@ -4,39 +4,39 @@ GO
 USE personal_library;
 
 CREATE TABLE Locations (
-ID int not null,
-City varchar(30) not null,
-State char(2) not null,
+ID int NOT NULL,
+City varchar(30) NOT NULL,
+State char(2) NOT NULL,
 PRIMARY KEY(ID));
 
 CREATE TABLE Publishers (
-ID int not null,
-Company varchar(60) not null,
-LocationID int not null,
+ID int NOT NULL,
+Company varchar(60) NOT NULL,
+LocationID int NOT NULL,
 PRIMARY KEY(ID),
 CONSTRAINT FK_Location FOREIGN KEY(LocationID) REFERENCES Locations(ID));
 
 CREATE TABLE Authors (
-ID int not null,
-Fname varchar(30) not null,
+ID int NOT NULL,
+Fname varchar(30) NOT NULL,
 Mname varchar(30) DEFAULT '',
-Lname varchar(30) not null,
+Lname varchar(30) NOT NULL,
 PRIMARY KEY(ID));
 
 CREATE TABLE Books (
-ISBN13 bigint(13) not null,
-Format varchar(9) not null DEFAULT 'paperback',
-Edition tinyint(2) not null DEFAULT 1,
-Pages mediumint(5) not null,
+ISBN13 bigint(13) NOT NULL,
+Format varchar(9) NOT NULL DEFAULT 'paperback',
+Edition tinyint(2) NOT NULL DEFAULT 1,
+Pages mediumint(5) NOT NULL,
 ISBN10 int(10),
-PublisherID int not null,
+PublisherID int NOT NULL,
 PRIMARY KEY(ISBN13),
 CONSTRAINT FK_Publisher FOREIGN KEY(PublisherID) REFERENCES Publishers(ID),
 CONSTRAINT UQ_ISBN10 UNIQUE(ISBN10));
 
 CREATE TABLE Details (
-ID int not null,
-Title varchar(60) not null,
+ID int NOT NULL,
+Title varchar(60) NOT NULL,
 CoverPath varchar(40),
 SynopsisPath varchar(40),
 PRIMARY KEY(ID),
@@ -46,27 +46,27 @@ CONSTRAINT UQ_SynopsisPath UNIQUE(SynopsisPath),
 CONSTRAINT UQ_TableOfContentsPath UNIQUE(TableOfContentsPath));
 
 CREATE TABLE Genres (
-ID int not null,
-Genre varchar(20) not null,
+ID int NOT NULL,
+Genre varchar(20) NOT NULL,
 PRIMARY KEY(ID));
 
 CREATE TABLE Book_Authors (
-BookISBN13 bigint(13) not null,
-AuthorID int not null,
+BookISBN13 bigint(13) NOT NULL,
+AuthorID int NOT NULL,
 CONSTRAINT PK_Book_Author PRIMARY KEY (BookISBN13,AuthorID),
 CONSTRAINT FK_Book FOREIGN KEY(BookISBN13) REFERENCES Books(ISBN13),
 CONSTRAINT FK_Author FOREIGN KEY(AuthorID) REFERENCES Authors(ID));
 
 CREATE TABLE Book_Genres (
-BookISBN13 bigint(13) not null,
-GenreID int not null,
+BookISBN13 bigint(13) NOT NULL,
+GenreID int NOT NULL,
 CONSTRAINT PK_Book_Genre PRIMARY KEY (BookISBN13, GenreID),
 CONSTRAINT FK_Book FOREIGN KEY(BookISBN13) REFERENCES Books(ISBN13),
 CONSTRAINT FK_Author FOREIGN KEY(GenreID) REFERENCES Genres(ID));
 
 CREATE TABLE Book_Details (
-BookISBN13 bigint(13) not null,
-DetailID int not null,
+BookISBN13 bigint(13) NOT NULL,
+DetailID int NOT NULL,
 CONSTRAINT PK_Book_Details PRIMARY KEY (BookISBN13, DetailID),
 CONSTRAINT FK_Book FOREIGN KEY(BookISBN13) REFERENCES Books(ISBN13),
 CONSTRAINT FK_Detail FOREIGN KEY(DetailID) REFERENCES Details(ID));
