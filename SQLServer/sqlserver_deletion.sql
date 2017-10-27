d1bf32011bd25a3c5a1afae09cad0de66ec39d2b
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-27 16:51:42 -0400

    Create sqlserver_deletion.sql Copy of mysql_deletion ported for SQL Server.

---
+++ SQLServer/sqlserver_deletion.sql
@@ -0,0 +1,45 @@
+USE personal_library;
+
+ALTER TABLE Publishers
+DROP CONSTRAINTY FK_Location;
+
+ALTER TABLE Books
+DROP CONSTRAINT FK_Publisher;
+
+ALTER TABLE Book_Authors
+DROP CONSTRAINT KEY FK_Book;
+
+ALTER TABLE Book_Authors
+DROP CONSTRAINT FK_Author;
+
+ALTER TABLE Book_Genres
+DROP CONSTRAINT FK_Book;
+
+ALTER TABLE Book_Genres
+DROP CONSTRAINT FK_Author;
+
+ALTER TABLE Book_Details
+DROP CONSTRAINT FK_Book;
+
+ALTER TABLE Book_Details
+DROP CONSTRAINT FK_Detail;
+
+DROP TABLE Locations;
+
+DROP TABLE Publishers;
+
+DROP TABLE Authors;
+
+DROP TABLE Books;
+
+DROP TABLE Details;
+
+DROP TABLE Genres;
+
+DROP TABLE Book_Authors;
+
+DROP TABLE Book_Genres;
+
+DROP TABLE Book_Details;
+
+DROP DATABASE personal_library;
